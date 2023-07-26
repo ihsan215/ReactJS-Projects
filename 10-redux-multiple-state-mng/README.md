bda6319e0259623a32c29183bc2df8d93a8f00db
ihsan215 <aliihsantas34@gmail.com>
2023-07-26 14:00:31 +0300

    Added Redux Multiple State Mng

---
+++ 10-redux-multiple-state-mng/README.md
@@ -0,0 +1,27 @@
+**_<center>React JS Projects Series | 10 - Redux | Multiple State Management </center>_**
+
+<br>
+
+In this demo, a multi-state management application has been made with the Redux framework. State management is done with redux over form input, user login applications, and counter app.
+
+The orginal project from [Academind](https://github.com/academind) React course
+
+<br>
+
+<p align="center">
+  <img width="500" src="src\img\App.png">
+  <br>
+</p>
+<br>
+
+The content of the Create User List application;
+
+- React JS
+- Hooks
+- Redux
+- Redux Toolkit
+- Custom hooks
+- JSX
+- Basic JS
+- Basic HTML
+- Basic CSS
---
+++ 10-redux-multiple-state-mng/src/store/index.js
@@ -0,0 +1,87 @@
+// import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+import counterSlice from "./counter.js";
+import authSlice from "./auth.js";
+
+// const initialCounterState = { counter: 0, showCounter: true };
+
+// const counterSlice = createSlice({
+//   name: "counter",
+//   initialState: initialCounterState,
+//   reducers: {
+//     increment(state) {
+//       state.counter++;
+//     },
+//     decrement(state) {
+//       state.counter--;
+//     },
+//     increase(state, action) {
+//       state.counter += action.payload;
+//     },
+//     toggleCounter(state) {
+//       state.showCounter = !state.showCounter;
+//     },
+//   },
+// });
+
+// const initialAuthState = {
+//   isAuthenticated: false,
+// };
+
+// const authSlice = createSlice({
+//   name: "auth",
+//   initialState: initialAuthState,
+//   reducers: {
+//     login(state) {
+//       state.isAuthenticated = true;
+//     },
+//     logout(state) {
+//       state.isAuthenticated = false;
+//     },
+//   },
+// });
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// const store = createStore(counterReducer);
+
+const store = configureStore({
+  reducer: { counter: counterSlice, auth: authSlice },
+});
+
+export const counterActions = counterSlice.actions;
+
+export const authActions = authSlice.actions;
+
+export default store;
